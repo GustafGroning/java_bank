d0ad5f014b5223700fed25d331a85f9262b6e9c6
Gustaf Gröning <gustafgroning@gmail.com>
2021-06-13 23:05:08 +0200

    init

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main { 
+// namnet på public class måste matcha namnet på filen du är i (eller åtminstone finnas i mappen)
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+        
+    }
+}
